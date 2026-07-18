f97d7460172097da72e7007aa838c4475222b050
Gonen101 <118046718+Gonen101@users.noreply.github.com>
2026-07-18 20:58:42 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import AuthGate from './AuthGate';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthGate />
-  </StrictMode>
-);
